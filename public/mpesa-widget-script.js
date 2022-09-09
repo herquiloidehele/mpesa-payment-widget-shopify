447ef01aba76488fc77fc604e24e1a0d358c3e9e
Herquiloide Hele <hhele@mail.wit-software.com>
2022-09-09 22:59:52 +0200

    update script tag

---
+++ public/mpesa-widget-script.js
@@ -0,0 +1,8 @@
+<script>
+  {%- if checkout.order.gateway contains 'M-pesa' -%}
+    Shopify.Checkout.OrderStatus.addContentBox(
+    '<h2>Efectuar Pagamento</h2>',
+    "<iframe style='width: 100%; height: 100%;' src='https://mpesa-payment-widget-shopify.vercel.app/?order={{checkout.order_id}}&shop={{shop.permanent_domain}}'></iframe>"
+    );
+  {%- endif -%}
+</script>
