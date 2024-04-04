5b8a74f4c814ae61cce3b559942eb32f77263c29
Herquiloide Hele <herquiloide.hele@wit-software.com>
2024-04-04 20:25:45 +0100

    text info

--- public/mpesa-widget-script.js
+++
@@ -1,4 +0,0 @@
-Shopify.Checkout.OrderStatus.addContentBox(
-  '<h2>Efectuar Pagamento</h2>',
-  "<iframe style='width: 100%; height: 100%;' src='https://mpesa-payment-widget-shopify.vercel.app/?order={{checkout.order_id}}&shop={{shop.permanent_domain}}'></iframe>"
-)
