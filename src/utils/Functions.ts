0b02ad6022f64e65596d7ab33b9d66c0ad44c180
Herquiloide Hele <herquiloide.hele@wit-software.com>
2025-01-11 08:56:52 +0000

    update mpesa tax function

--- src/utils/Functions.ts
+++ src/utils/Functions.ts
@@ -2,7 +2,7 @@ import { MPESA_TAX } from '@/utils/Constants'
 
 export function getValueWithTax(value: number) {
   const valueAmount = Number(value)
-  return formatCurrency(Math.round(valueAmount + valueAmount * MPESA_TAX))
+  return formatCurrency(Math.ceil(Math.round(valueAmount) / (1 - MPESA_TAX)))
 }
 
 export const formatCurrency = (price: number) => {
